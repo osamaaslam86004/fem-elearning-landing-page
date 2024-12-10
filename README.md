9c6e4a6bfe50dd54d165718c73f12675a9436e7f
osamaaslam86004 <osama.aslam.86004@gmail.com>
2024-12-10 23:33:47 +0500

    animations/crypto.html

---
+++ README.md
@@ -0,0 +1,3 @@
+## Landing page
+
+![e-learning landing page](screencapture-elearning-landing-page-three-vercel-app-2024-12-07-18_33_55.png)
---
+++ js/aos-init.js
@@ -0,0 +1,19 @@
+
+
+  // Initialize AOS
+  AOS.init({
+    duration: 1200, // Animation duration
+    offset: 100, // Trigger offset in pixels
+    easing: "ease-in-out", // Animation easing
+    once: false, // Animation happens only once
+      // Global settings:
+    disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+    startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+    initClassName: 'aos-init', // class applied after initialization
+    animatedClassName: 'aos-animate', // class applied on animation
+    useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+    disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+    debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+    throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+    mirror: true,
+  });
